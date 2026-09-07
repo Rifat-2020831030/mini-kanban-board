b3d5fe3cdbbdf685d46651d8f1a2ffcc48c5a58e
Ben <code.hasan@protonmail.com>
2026-09-07 18:01:49 +0600

    fix(chore): cloud db migration and prisma config update

--- kanban-server/prisma.config.ts
+++ kanban-server/prisma.config.ts
@@ -1,11 +1,11 @@
-import "dotenv/config";
 import { defineConfig, env } from "@prisma/config";
+import "dotenv/config";
 import type { PrismaConfig } from "prisma";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_URL"),
   },
   migrations: {
     path: "prisma/migrations",
